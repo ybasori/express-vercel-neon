5f1a100c0e50b526ebd1fac460a519d39dbf7b0a
YUSUF PC <ybasori@gmail.com>
2025-07-31 12:46:33 +0700

    first commit

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,31 @@
 import express from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.status(200).json({
-    message: "hello",
-  });
+// Set up PostgreSQL pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  },
 });
+
+app.get("/", async (req, res) => {
+  try {
+    const {rows:result} = await pool.query("SELECT NOW()");
+
+    return res.status(200).json({
+      message: "hello",
+      result,
+    });
+  } catch (err:any) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
 export default app;
